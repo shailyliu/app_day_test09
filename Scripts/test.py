0668394cad8faef46f76ab84926e98d3d99edb90
shailyliu <1277819978@qq.com>
2018-07-10 15:18:56 +0800

    updata xinxi

--- Scripts/test.py
+++ Scripts/test.py
@@ -1,6 +1,14 @@
+import allure
 class Test_aa:
 
-    def test_abc(self):
+    def test_01(self):
+        allure.attach("描述","描述内容")
+        with open("./Screen/test_0011.png","rb")as f:
+            allure.attach("截图名字",f.read(),allure.attach_type.PNG)
+    def test_002(self):
+        assert 1,self.test_01()
 
-        assert 0,"hahahahha"
+    def test_003(self):
+        assert 0,self.test_01()
+
 
